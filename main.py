1ba4bdf4c5fe8e4797f9d66583fa32af31dfafce
gratusrichard <gratusri@gmail.com>
2025-04-24 12:44:24 +0100

    'typo'

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from fastapi.middleware.wsgi import WSGIMiddleware
 import your_dash_script_name  # replace with the name of your .py file without `.py`
 
 app = FastAPI()
-dash_app = your_dash_script_name.app  # the Dash instance from your script
+dash_app = app.app  # the Dash instance from your script
 app.mount("/", WSGIMiddleware(dash_app.server))
